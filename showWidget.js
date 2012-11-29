4810a2773c151de0c0a43cf6d79517d8767fbcce
Alex Muench <ammuench@gmail.com>
2012-11-29 10:50:26 -0500

    Started logic
    Got most of the logic framework written up, added images to test that
    out.
    For some reason parseXML is broken in Chrome again, but works in IE and
    FF.
    First for loop in the logic never starts, needs to be fixed

--- showWidget.js
+++ showWidget.js
@@ -1,4 +1,15 @@
+//global variables
+var dayArray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+var dateTime = new Date();
+var currentMinutes;
+var showNumbers = new Array();
+var currentShow;
+
+//startup code
 window.onload = pageInit;
+function $(id) { return document.getElementById(id); }
+//logic start
+//xml loading script
 
 function parseXML() 
 { 
@@ -13,8 +24,70 @@ function pageInit()
 	alert("preInitLibary");
 	parseXML();
 	alert("XML Loaded");
-	var dayTag = xmlDoc.getElementsByTagName('day')[0];
-	var dayValues = dayTag.childNodes[0];
-    var text = dayValues.nodeValue;
-	alert("The day value is " + text);
+	var dayTag = xmlDoc.getElementsByTagName('day');
+	var currentDay = dateTime.getUTCDay();
+	generateCurrentTime();
+	alert('time generated');
+	//adjusts day to eastern time from UTC
+	if (currentMinutes > 1260)
+	{
+		currentDay--;
+		if (currentDay < 0)
+		{
+			currentDay = 7;
+		}
+	}
+	alert("starting daytag loop");
+	//gets list of all shows with matching date
+	for (i=0; i<dayTag.length; i++)
+	{
+		alert("Current day = " + dayArray[currentDay] + " Show Day = " + dayTag[i].childNodes[0].nodeValue);
+		if (dayTag[i].childNodes[0].nodeValue == dayArray[currentDay])
+		{
+			showNumbers.push(i)
+		}
+	}
+	//compares times of all shows with correct day, tries to find the right show
+	var startTimes = xmlDoc.getElementsByTagName('starttime');
+	var endTimes = xmlDoc.getElementsByTagName('endtime');
+	var isDefault = true;
+	for (j=0; j<showNumbers.length; j++)
+	{
+		if (currentMinutes >= startTimes[showNumbers[j]].childNodes[0].nodeValue && currentMinutes < endTimes[showNumbers[j]].childNodes[0].nodeValue )
+		{
+			isDefault = false;
+			currentShow = showNumbers[j];
+
+		}
+	}
+	//sets default case if true
+	if (isDefault == true)
+	{
+		setDefault();
+	}else{
+		setCurrent();
+	}
+}
+
+function generateCurrentTime()
+{
+	currentMinutes = dateTime.getUTCHours() * 60;
+	currentMinutes += dateTime.getUTCMinutes();
+	currentMinutes -= 300;
+}
+
+function setDefault(){
+	$("showPic").setAttribute("src", xmlDoc.getElementById('defaultImage').childNodes[0].nodeValue);
+	$("showTitle").innerHTML = xmlDoc.getElementById('defaultTitle').childNodes[0].nodeValue;
+	$("djNames").innerHTML = xmlDoc.getElementById('defaultDJ').childNodes[0].nodeValue;
+	$("showTime").innerHTML = xmlDoc.getElementById('defaultShowTime').childNodes[0].nodeValue;
+	$("showInfo").innerHTML = xmlDoc.getElementById('defaultInfo').childNodes[0].nodeValue;
+}
+
+function setCurrent(){
+	$("showPic").setAttribute("src", xmlDoc.getElementsByTagName('showpic')[currentShow].childNodes[0].nodeValue);
+	$("showTitle").innerHTML = xmlDoc.getElementsByTagName('showtitle')[currentShow].childNodes[0].nodeValue;
+	$("djNames").innerHTML = xmlDoc.getElementsByTagName('djs')[currentShow].childNodes[0].nodeValue;
+	$("showTime").innerHTML = xmlDoc.getElementsByTagName('showtime')[currentShow].childNodes[0].nodeValue;
+	$("showInfo").innerHTML = xmlDoc.getElementsByTagName('showinfo')[currentShow].childNodes[0].nodeValue;
 }
